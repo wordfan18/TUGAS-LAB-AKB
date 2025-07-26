d693b8b088b91157945ecfc67a681e26e1115dd4
pandafrika <105841106322@student.unismuh.ac.id>
2025-07-27 02:30:02 +0800

    TUGAS 6

--- app/index.tsx
+++ app/index.tsx
@@ -1,47 +1,27 @@
-import { ScrollView, Text, View } from "react-native";
+import React from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Index() {
   return (
-    <ScrollView contentContainerStyle={{ padding: 20 }}>
-      <Text style={{ fontFamily: "bitcount-double", fontSize: 26, color: "blue" }}>
-        Juliani{"\n"}105841104022
-      </Text>
+    <ScrollView contentContainerStyle={{ padding: 20, flexGrow: 1 }}>
+      <Text style={{ fontSize: 40, marginBottom: 10 }}>Daftar 10 Ikon:</Text>
 
-      <Text style={{ fontFamily: "bitcount-single", fontSize: 24 }}>
-        Azzah Aulia{"\n"}105841105022
-      </Text>
-
-      <Text style={{ fontFamily: "bungee", fontSize: 24 }}>
-        Syauqiyah Mujahidah Yahya{"\n"}105841105122
-      </Text>
-
-      <Text style={{ fontFamily: "exo2-bolditalic", fontSize: 24 }}>
-        Mar'atul Azizah{"\n"}105841105222
-      </Text>
-
-      <Text style={{ fontFamily: "intertight-bolditalic", fontSize: 24 }}>
-        Fikrah Lejahtegis{"\n"}105841105322
-      </Text>
-
-      <Text style={{ fontFamily: "opensans-italic", fontSize: 24 }}>
-        Alya{"\n"}105841105422
-      </Text>
-
-      <Text style={{ fontFamily: "oswald-variable", fontSize: 24 }}>
-        M. Fikri Haikal Ayatullah{"\n"}105841105522
-      </Text>
-
-      <Text style={{ fontFamily: "outfit-medium", fontSize: 24 }}>
-        Muh. Dirham Rahim{"\n"}105841105622
-      </Text>
-
-      <Text style={{ fontFamily: "playfair-italic", fontSize: 24 }}>
-        Zaskya Aulia Ashar{"\n"}105841105722
-      </Text>
-
-      <Text style={{ fontFamily: "robotocondensed-italic", fontSize: 24 }}>
-        Muh. Ilham Akbar{"\n"}105841105822
-      </Text>
+      <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between" }}>
+        <AntDesign name="stepforward" size={80} color="black" />
+        <AntDesign name="stepbackward" size={80} color="black" />
+        <Entypo name="infinity" size={80} color="black" />
+        <FontAwesome name="plane" size={80} color="black" />
+        <Feather name="bell" size={80} color="black" />
+        <Feather name="calendar" size={80} color="black" />
+        <Feather name="activity" size={80} color="black" />
+        <Feather name="airplay" size={80} color="black" />
+        <FontAwesome name="heart" size={80} color="black" />
+        <FontAwesome name="star" size={80} color="black" />
+      </View>
     </ScrollView>
   );
 }
